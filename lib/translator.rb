f8aba581121c906c91de7b7eb9a4e6747b9c9a0b
Elias Campos <ecampose76@gmail.com>
2020-11-18 04:21:23 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -7,6 +7,8 @@ def load_library(path)
 final_hash = {}
 YAML.load_file(path).each do |key, value|
   if !final_hash[key] = {}
+    final_hash[key][:english] = ""
+    final_hash[key][:japanese] = ""
     end
     binding.pry
 end
