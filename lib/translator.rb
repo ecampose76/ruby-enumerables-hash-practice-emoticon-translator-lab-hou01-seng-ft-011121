81d5c8847c07614f5bd92aefe0a57b6da94553af
Elias Campos <ecampose76@gmail.com>
2020-11-18 05:24:23 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -22,6 +22,7 @@ def get_japanese_emoticon(path, emoticon)
       return value[:japanese]
     end
   end
+  return "Sorry, that emoticon was not found"
 end
 
 def get_english_meaning(path, emoticon)
